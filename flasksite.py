32d2221c9de7be5237e7a4d39d311d9c0298c9a5
pizzaman43 <will.confrey@gmail.com>
2025-07-28 11:16:43 -0500

    added new image

--- flasksite.py
+++ flasksite.py
@@ -203,7 +203,7 @@ CONTACT_HTML = (
     + """
     <div class="container">
       <h1>Contact the Owner</h1>
-      <img src="/static/IMG_7079.jpg" alt="Dog with collar" class="contact-img">
+      <img src="static\IMG_5463.jpeg" alt="Dog with collar" class="contact-img">
       <div class="contact-info">
         <strong>Confrey family</strong><br>
         """
